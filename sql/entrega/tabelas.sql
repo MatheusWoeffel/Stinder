f2caaa7a3403204a4e1cc6ea366bc1e2bdfb3c33
MatheusWoeffel <mwcamargo@inf.ufrgs.br>
2020-09-06 21:31:30 -0300

    Fix trigger

--- sql/entrega/tabelas.sql
+++ sql/entrega/tabelas.sql
@@ -168,7 +168,7 @@ CREATE TABLE Card(
 
 CREATE FUNCTION createPhotoActivity() RETURNS TRIGGER AS $$
 BEGIN
-  INSERT INTO Activity(userId,type,game,photo) VALUES (OLD.userId,'p',NULL,OLD.url);
+  INSERT INTO Activity(userId,type,game,photo) VALUES (OLD.userId,'p',NULL,OLD.id);
   RETURN NULL; --Gatilho after
 END
 $$ LANGUAGE 'plpgsql';
@@ -182,7 +182,7 @@ EXECUTE PROCEDURE createPhotoActivity();
 
 CREATE FUNCTION createGameActivity() RETURNS TRIGGER AS $$
 BEGIN
-  INSERT INTO Activity(userId,type,game,photo) VALUES (OLD.userId,'g',(SELECT thumbnail FROM game WHERE id=OLD.game),NULL);
+  INSERT INTO Activity(userId,type,game,photo) VALUES (OLD.userId,'g',OLD.game,NULL);
   RETURN NULL; --Gatilho after
 END
 $$ LANGUAGE 'plpgsql';
